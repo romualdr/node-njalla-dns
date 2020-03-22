ebaf9c993d4c41f3ca7d3316423beb0c99d15a47
Romuald R <dafunix@gmail.com>
2020-03-22 18:13:58 +0100

    [fix] #2 Reworked parsing to avoid security concerns about VM [feat] Handling backslashes (+tests) [feat] Handling various parsing errors like spaces, semicolons ... (+test) [feat] 1.0.5

--- tests/lib/getRecords.js
+++ tests/lib/getRecords.js
@@ -31,10 +31,46 @@ test('getRecords: should parse complex records ( like DKIM with ; )', async t =>
     t.truthy(records.find((r) => r.name === 'myrecord'), 'Record was not found')
 })
 
+test('getRecords: should parse DKIM records with slashes in it', async t => {
+    await mockedClient.addMock(mockedClient.GET_NJALLA_URL('/signin/'), 'signin.html')
+    await mockedClient.addMock(mockedClient.GET_NJALLA_DOMAIN_URL('dkim_with_slashes.io'), 'getRecords.dkim_with_slashes.html')
+
+    await dns.login('', '')
+    const records = await dns.getRecords('dkim_with_slashes.io')
+    t.truthy(records.find((r) => r.name === 'myrecord'), 'Record was not found')
+})
+
+test('getRecords: should parse records with space between ; and the end of the line', async t => {
+    await mockedClient.addMock(mockedClient.GET_NJALLA_URL('/signin/'), 'signin.html')
+    await mockedClient.addMock(mockedClient.GET_NJALLA_DOMAIN_URL('spaces_at_the_end.io'), 'getRecords.spaces_at_the_end.html')
+
+    await dns.login('', '')
+    const records = await dns.getRecords('spaces_at_the_end.io')
+    t.truthy(records.find((r) => r.name === 'myrecord'), 'Record was not found')
+})
+
+test('getRecords: should parse records without semicolon at the end', async t => {
+    await mockedClient.addMock(mockedClient.GET_NJALLA_URL('/signin/'), 'signin.html')
+    await mockedClient.addMock(mockedClient.GET_NJALLA_DOMAIN_URL('no_semicolon.io'), 'getRecords.no_semicolon.html')
+
+    await dns.login('', '')
+    const records = await dns.getRecords('no_semicolon.io')
+    t.truthy(records.find((r) => r.name === 'myrecord'), 'Record was not found')
+})
+
+test('getRecords: should parse records without semicolon & dkim & slashes & spaces', async t => {
+    await mockedClient.addMock(mockedClient.GET_NJALLA_URL('/signin/'), 'signin.html')
+    await mockedClient.addMock(mockedClient.GET_NJALLA_DOMAIN_URL('dkim_slashes_no_sc_spaces.io'), 'getRecords.dkim_slashes_no_sc_spaces.html')
+
+    await dns.login('', '')
+    const records = await dns.getRecords('dkim_slashes_no_sc_spaces.io')
+    t.truthy(records.find((r) => r.name === 'myrecord'), 'Record was not found')
+})
+
 test('getRecords: should parse multiple mixed scripts with simple and complex records', async t => {
     await mockedClient.addMock(mockedClient.GET_NJALLA_URL('/signin/'), 'signin.html')
     await mockedClient.addMock(mockedClient.GET_NJALLA_DOMAIN_URL('double.io'), 'getRecords.double.html')
-    
+
     await dns.login('', '')
     const records = await dns.getRecords('double.io')
     t.truthy(records.find((r) => r.name === 'simple'), 'Record was not found')
